2c1398693116017dbdf9bb12978439154043376a
fujitani sora <fujitanisora0414@gmail.com>
2026-07-09 15:20:43 +0900

    Update rt to v0.0.2

--- Formula/rt.rb
+++ Formula/rt.rb
@@ -1,8 +1,8 @@
 class Rt < Formula
   desc "Run Ruby-defined tasks from a discoverable CLI"
   homepage "https://github.com/sorafujitani/rt"
-  url "https://github.com/sorafujitani/rt/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "2e72a49f2cbe5945fb3d5bac690e525783f3a1215ffc9cfdd287a2622a091f30"
+  url "https://github.com/sorafujitani/rt/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "87a5b42827e9632fa626f86c2c184d65e63a358e527c200e0831df8f043cc3e3"
   license "MIT"
   head "https://github.com/sorafujitani/rt.git", branch: "main"
 
