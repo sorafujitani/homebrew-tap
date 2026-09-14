24c62963b6b6253c8bfe2900c483b9e8a7032371
fujitani sora <fujitanisora0414@gmail.com>
2026-09-14 14:12:46 +0900

    Distribute linearctl 0.1.3 macOS bottles

--- Formula/linearctl.rb
+++ Formula/linearctl.rb
@@ -6,6 +6,13 @@ class Linearctl < Formula
   license "MIT"
   head "https://github.com/sorafujitani/linearctl.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/sorafujitani/homebrew-tap/releases/download/linearctl-v0.1.3"
+    rebuild 1
+    sha256 cellar: "/opt/homebrew/Cellar", arm64_sonoma: "094b505285a02bcb25c14996430b737bdad30d35b85e5c0bbc9527d711e06579"
+    sha256 cellar: :any_skip_relocation, sequoia: "fd7c9d4c0b7cbfff26fe5b01f26113502b85bc81f75bdf52f6f1c758fc63158e"
+  end
+
   depends_on "bun" => :build
 
   def install
